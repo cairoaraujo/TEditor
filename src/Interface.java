54d59d465abbb7dc9403a99397b19c07458cc849
Cairo Araujo <cairobezerra@hotmail.com>
2021-09-08 19:00:16 -0300

    some changes - save and save as methods added

--- src/Interface.java
+++ src/Interface.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -10,17 +11,19 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Interface implements ActionListener {
-	
+	JTextField path;
 	JFrame window;
 	JTextArea textArea;
 	JScrollPane scroll;
 	JMenuBar menuBar;
 	JMenu menuFile;
-	JMenuItem New, Open, Save;
+	JMenuItem New, Open, Save, SaveAs;
 	JFrame frame;
 	JPanel statusPanel;
+	JTextField textField;
 	MenuFunctions file = new MenuFunctions(this);//funcao para o menu "New"
 	
 	public static void main(String[] args) {
@@ -33,7 +36,11 @@ public class Interface implements ActionListener {
 		window.setVisible(true);
 		createMenuBar();
 		fileMenuItens();
-		createStateBar();
+		//createStateBar();
+		textField = new JTextField();
+		textField.setEditable(false);
+		scroll.setColumnHeaderView(textField);
+		
 		
 	}
 	
@@ -58,9 +65,10 @@ public class Interface implements ActionListener {
 	}
 	
 	public void createStateBar() {//barra na borda inferior que mostra o path do arquivo aberto
-		frame = new JFrame();
-		frame.setLayout(new BorderLayout());
-		frame.setSize(200,200);
+		BorderLayout stateBar = new BorderLayout();
+		JLabel stateBarLabel = new JLabel();
+		window.add(path,stateBar.SOUTH);
+		
 		
 		statusPanel = new JPanel();
 		
@@ -76,10 +84,17 @@ public class Interface implements ActionListener {
 		Open.setActionCommand("Open");
 		
 		Save = new JMenuItem("Save");
+		Save.addActionListener(this);
+		Save.setActionCommand("Save");
+		
+		SaveAs = new JMenuItem("Save as");
+		SaveAs.addActionListener(this);
+		SaveAs.setActionCommand("Save as");
 		
 		menuFile.add(New);
 		menuFile.add(Open);
 		menuFile.add(Save);
+		menuFile.add(SaveAs);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -88,6 +103,8 @@ public class Interface implements ActionListener {
 		switch(command) {
 			case "New": file.newFile(); break;
 			case "Open": file.open(); break;
+			case "Save as": file.saveAs(); break;
+			case "Save": file.save(); break;
 		}
 	}
 
--- src/MenuFunctions.java
+++ src/MenuFunctions.java
@@ -1,6 +1,18 @@
+import java.awt.BorderLayout;
 import java.awt.FileDialog;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 
 public class MenuFunctions {
 	Interface gui;
@@ -15,16 +27,49 @@ public class MenuFunctions {
 		
 		gui.textArea.setText("");
 		gui.window.setTitle("New - TEditor");
+		fileName = null;
+		filePath = null;
 	}
 	
 	public void open() {
-		FileDialog fd = new FileDialog(gui.window, "Choose a txt file to open on TEditor", FileDialog.LOAD);
+		System.out.print("APERTEI");
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setDialogTitle("Select a file:");
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Text file", "txt");
+		
+		fileChooser.setFileFilter(filter);
+		fileChooser.showOpenDialog(fileChooser);
+		fileChooser.setVisible(true);
+		
+		File file = new File(fileChooser.getSelectedFile().toString());
+		
+		if (file != null) {
+			filePath = file.getPath();
+			fileName = file.getName();
+			gui.window.setTitle(fileName);
+
+		}
+		try {
+			Scanner scanner = new Scanner(file);
+			while(scanner.hasNextLine()) {
+				String data = scanner.nextLine();
+				gui.textArea.insert(data, 0);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("The file cannot be opened. Check if is correct.");
+		}
+		
+		/*FileDialog fd = new FileDialog(gui.window, "Choose a txt file to open on TEditor", FileDialog.LOAD);
 		fd.setVisible(true);
 		
 		if(fd.getFile() != null) {
 			fileName = fd.getFile();
 			filePath = fd.getDirectory();
 			gui.window.setTitle(fileName);
+			BorderLayout stateBar = new BorderLayout();
+			JLabel stateBarLabel = new JLabel(filePath);
+			gui.window.add(stateBarLabel,stateBar.SOUTH);
 		}
 		
 		try {
@@ -39,6 +84,58 @@ public class MenuFunctions {
 			br.close();
 		}catch(Exception e) {
 			System.out.println("The file cannot be opened. Check if is correct.");
+		}*/
+	}
+	
+	public void save() {
+		JFileChooser chooser = new JFileChooser();
+		int i = chooser.showSaveDialog(chooser);
+		if(i!=JFileChooser.APPROVE_OPTION)return;
+		try {
+			BufferedWriter bf = new BufferedWriter(new FileWriter(chooser.getSelectedFile()+".txt"));
+			bf.write(gui.textArea.getText());
+			bf.flush();
+			bf.close();
+		}catch(IOException el){
+			el.printStackTrace();
+		}
+		//FUNCAO SAVE PRECISA MELHORAR!!!
+		/*if (fileName == null) {
+			System.out.print("DEU RUIM");
+			saveAs();
+			
+		}
+		else {
+			try {
+				System.out.print(fileName);
+				FileWriter fw = new FileWriter (fileName);
+				String text = gui.textArea.getText();
+				fw.write(text);
+				fw.close();
+				
+			}catch(Exception e){
+				System.out.println("Error!");
+			}
+		}*/
+	}
+	
+	public void saveAs() {
+		JFileChooser saveChooser = new JFileChooser();
+		saveChooser.showSaveDialog(saveChooser);
+		File savingFile = saveChooser.getSelectedFile();
+		
+		try {
+			filePath = savingFile.getPath();
+			fileName = savingFile.getName();
+			gui.window.setTitle(fileName);
+			FileWriter fw = new FileWriter (savingFile);
+			String text = gui.textArea.getText();
+			fw.write(text);
+			fw.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
